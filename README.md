fa7bc944f14b2133cc4fa3ca8fdbc9cb389b1153
James Gruber <jgrube01@calpoly.edu>
2024-05-11 15:00:32 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#REAMDE for kernel hacking base setup
+- linux - Linux kernel source code
+- buildroot - Buildroot config for userspace
+- root - stuff to be placed in root filesystem
---
+++ run_qemu.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+qemu-system-x86_64 -kernel linux/arch/x86/boot/bzImage -boot c -m 512 -hda buildroot/output/images/rootfs.ext4 -append "root=/dev/sda rw console=ttyS0" -serial stdio -display none
