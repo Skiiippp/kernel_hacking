45a41e26705ce6711df5c6c7582855cfda90eb0c
James Gruber <jgrube01@calpoly.edu>
2024-05-12 23:26:03 -0700

    change ctrl-c bind so it works inside qemu

--- run_qemu.sh
+++ run_qemu.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+stty intr ^]
 qemu-system-x86_64 -kernel linux/arch/x86/boot/bzImage -boot c -m 512 -hda buildroot/output/images/rootfs.ext4 -append "root=/dev/sda rw console=ttyS0" -serial stdio -display none
